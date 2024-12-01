bdb84ef812c183a1b64030e1a08dc03bb08f585b
Pavel Latyshov <dev@latyshov.com>
2024-12-01 15:17:43 +0300

    extend obj test

--- src/object.js
+++ src/object.js
@@ -1,4 +1,5 @@
 /* ========================================================================= */
+// extendObject and update keys
 function extendObjectAndReplace(obj1, obj2){
 
   for (let prop in obj2){
@@ -9,5 +10,39 @@ function extendObjectAndReplace(obj1, obj2){
 }
 
 /* ========================================================================= */
+// extendObject with non-exist keys
+function extendObject(obj1, obj2){
+
+  for (let prop in obj2){
+      if (!obj1[prop]) obj1[prop] = obj2[prop];
+  }
+
+  return obj1;
+}
+
+
+/* ========================================================================= */
 // Objects exports
 exports.extendObjectAndReplace = extendObjectAndReplace;
+exports.extendObject = extendObject;
+
+/* ========================================================================= */
+// Objects tests
+
+/*
+  const {extendObjectAndReplace} = require('./src/object');
+
+  const a = {a: 1}
+  const b = {a: 2, b: 3}
+  extendObjectAndReplace(a, b);
+  console.log(a, b) // { a: 2, b: 3 } { a: 2, b: 3 }
+*/
+
+/*
+  const {extendObject} = require('./src/object');
+
+  const a = {a: 1}
+  const b = {a: 2, b: 3}
+  extendObject(a, b);
+  console.log(a, b) // { a: 1, b: 3 } { a: 2, b: 3 }
+ */
