7948c3490ef32332055d44899dc639dfeb4a4d03
Pavel Latyshov <dev@latyshov.com>
2024-12-01 14:52:33 +0300

    shuffle test. Array split by chunk + test

--- src/array.js
+++ src/array.js
@@ -20,5 +20,55 @@ function shuffle(array) {
 };
 
 /* ========================================================================= */
+//Функция равномерного деления массива на chunks
+// splitArrayTo([a,b,c,d,e], 3) => [[a,b,c], [d,e]] 
+const splitArray = (inputArray, itemsPerChunk) => {
+    
+  var result = inputArray.reduce((resultArray, item, index) => { 
+    const chunkIndex = Math.floor(index/itemsPerChunk)
+  
+    if(!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = [] // start a new chunk
+    }
+  
+    resultArray[chunkIndex].push(item)
+  
+    return resultArray
+  }, []);
+
+  return result;
+};
+
+/* ========================================================================= */
 // Array exports
 exports.shuffle = shuffle;
+exports.splitArray = splitArray;
+
+/* ========================================================================= */
+// Array tests
+
+/*
+  const {shuffle} = require('./src/array');
+
+  let arr = [
+    1, 2, 3,
+    4, [1,2],
+    {5: 6}, 7, 8,
+    9, 10 
+  ]
+  console.log(shuffle(arr)) // [ 8, 9, 2, [ 1, 2 ], 10, 4, 3, 1, { '5': 6 }, 7 ]
+  console.log(shuffle(arr)) // [ [ 1, 2 ], 2, 1, 4, 3, 8, 7, 9, 10, { '5': 6 } ]
+*/
+
+/*
+  const {splitArray} = require('./src/array');
+
+  let arr = [
+    1, 2, 3,
+    4, [1,2],
+    {5: 6}, 7, 8,
+    9, 10 
+  ]
+  console.log(splitArray(arr, 2)) // [ [ 1, 2 ], [ 3, 4 ], [ [ 1, 2 ], { '5': 6 } ], [ 7, 8 ], [ 9, 10 ] ]
+  console.log(splitArray(arr, 5)) // [ [ 1, 2, 3, 4, [ 1, 2 ] ], [ { '5': 6 }, 7, 8, 9, 10 ] ]
+*/
