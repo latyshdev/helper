66d92b8099158986f797b5d282714fda4cd10a87
Pavel Latyshov <dev@latyshov.com>
2024-12-03 13:15:16 +0300

    browserStatusBySN

--- src/AdsPower/index.js
+++ src/AdsPower/index.js
@@ -1,2 +1,21 @@
-const {request} = require('./../request.js');
+const {axiosRequest} = require('./../request.js');
 const Config = require('./../../configs/ads.json');
+
+/* ========================================================================= */
+const browserStatusBySN = async (userid) => {
+  try {
+    const requestConfig = {};
+    requestConfig.method = `GET`;
+    const url = `${Config.API_ENDPOINT}:${Config.API_PORT}` + 
+      `/api/v1/browser/active?serial_number=${userid}`;
+    const response = await axiosRequest(url, requestConfig);
+    if (response?.data?.data) return response.data.data;
+    return null;
+  } catch (err) {
+      throw err.message;
+  }
+};
+
+/* ========================================================================= */
+// ADS exports
+exports.browserStatusBySN = browserStatusBySN;
